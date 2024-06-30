aabaf03f7f2eb83c2697df3c929c9c1f4da36e55
HAYA <purogram837@gmail.com>
2024-06-30 09:42:50 +0900

    トップ画面のボタンを押すと、通し動画の正面、斜めが切り替わる機能を追加

--- script/script.js
+++ script/script.js
@@ -15,3 +15,41 @@ window.addEventListener('load', () => {
   loadingLeft.animate(keyframes, options);
   loadingRight.animate(keyframes, options);
 })
+
+/* ボタンの切り替え */
+const toshi_url = {
+  doutyuu_fraont: "https://www.youtube.com/embed/1hQEgl_sfSk?si=vl3guWWwnxL6nc3i",
+  doutyuu_diagonal: "https://www.youtube.com/embed/XR9-YH3Er8E?si=Ps5_YrRbAuo5-npi",
+  dewa_fraont: "https://www.youtube.com/embed/RIODvZCKc9c?si=ONhTINRnauf1pyHy",
+  dewa_diagonal: "https://www.youtube.com/embed/GLB-OUOHed0?si=4YzPZa4SS4eafzON",
+  hiraniwa_fraont: "https://www.youtube.com/embed/TnFeCoOJayk?si=oYtPx9rg6CMDuM8a",
+  hiraniwa_diagonal: "https://www.youtube.com/embed/jxPujh6F5tw?si=oSdO_6VKqrutCaCx" 
+}
+// 通し動画 正面/ななめのurlオブジェクト
+const change = (btn, iframe, fraont_url, diagonal_url) => {
+  if (btn.textContent === '正面') {
+    btn.textContent = 'ななめ';
+    iframe.src = diagonal_url;
+  } else {
+    btn.textContent = '正面';
+    iframe.src = fraont_url;
+  }
+}
+// 道中太鼓_正面とななめの切り替え
+const btn1 = document.querySelector('.btn1');
+const iframe1 = document.querySelector('.iframe1');
+btn1.addEventListener('click', () => {
+  change(btn1, iframe1, toshi_url.doutyuu_fraont, toshi_url.doutyuu_diagonal);
+});
+// 出羽_正面とななめの切り替え
+const btn2 = document.querySelector('.btn2');
+const iframe2 = document.querySelector('.iframe2');
+btn2.addEventListener('click', () => {
+  change(btn2, iframe2, toshi_url.dewa_fraont, toshi_url.dewa_diagonal);
+});
+// 平庭_正面とななめの切り替え
+const btn3 = document.querySelector('.btn3');
+const iframe3 = document.querySelector('.iframe3');
+btn3.addEventListener('click', () => {
+  change(btn3, iframe3,toshi_url.hiraniwa_fraont,toshi_url.hiraniwa_diagonal);
+});
