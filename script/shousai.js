96ef943bf99b2827d96ec124228ff1afb5336887
HAYA <purogram837@gmail.com>
2024-09-08 11:54:53 +0900

    おどり方の基本でunderLineをつけた

--- script/shousai.js
+++ script/shousai.js
@@ -1,21 +1,29 @@
-const point = document.querySelector('.shousai-point');
+// const point = document.querySelector('.shousai-point');
 
-//監視対象が範囲内に入ったときにする動作
-const showPoint = (entries) => {
-  console.log(entries[0])
-  entries[0].target.animate(
-    {
-      opacity:[0,1],
-      rotate:['x 360deg',0],
-    },
-    {
-      duration:2000,
-      easing:'ease'
-    }
-  )
-};
+//監視対象が範囲内に入ったときにする動作(スライド)
+// const showPoint = (entries) => {
+//   entries[0].target.animate(
+//     {
+//       opacity:[0,1],
+//       translate:['0 30px',0],
+//     },
+//     {
+//       duration:2000,
+//       easing:'ease'
+//     }
+//   )
+// };
+// const pointObserver = new IntersectionObserver(showPoint);
+// pointObserver.observe(point);
 
-//監視ロボットの設定
-const pointObserver = new IntersectionObserver(showPoint);
-pointObserver.observe(point);
+//監視対象が範囲内に入ったときにする動作(下線を引く)
+const article = document.querySelector('.shousai-point-article');
+console.log(article);
+
+const underLine = (entries) =>{
+  entries[0].target.classList.add('on');
+}
+
+const articleObserver = new IntersectionObserver(underLine);
+articleObserver.observe(article);
 
