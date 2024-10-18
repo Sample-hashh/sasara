38e7862f33303bab8070b81470357c02e19e1520
HAYA <purogram837@gmail.com>
2024-10-19 01:23:49 +0900

    構えスマホ用作成。弓の引き方レイアウト崩れた

--- script/kamae.js
+++ script/kamae.js
@@ -1,28 +1,31 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const lists = document.querySelectorAll(".list");
+const kaisetu = document.querySelector(".shousai-kamae-bad");
 let count = 0;
 // 「<がクリックされたら、画像が1つ戻る
-prev.addEventListener("click",() =>{
+prev.addEventListener("click", () => {
   count--;
-  if(count < 0){
+  if (count < 0) {
     count = 1;
   }
   prev_next();
   changeColor(lists);
+  changeText(count);
 });
 // 「>がクリックされたら、画像が1つ進む
 next.addEventListener("click", () => {
   count++;
-  if(count > 1){
+  if (count > 1) {
     count = 0;
   }
   prev_next();
   changeColor(lists);
-})
+  changeText(count);
+});
 //インジケータの色が変わる関数
-const changeColor = (lists) =>{
-  switch(count){
+const changeColor = (lists) => {
+  switch (count) {
     case 0:
       lists[0].style.backgroundColor = "#000";
       lists[1].style.backgroundColor = "#fff";
@@ -32,10 +35,22 @@ const changeColor = (lists) =>{
       lists[1].style.backgroundColor = "#000";
       break;
   }
-}
+};
 
 const prev_next = () => {
   const img = document.querySelector(".bati_bad_img");
   const i = count;
-  img.src = `../images/kamae/bati_bad${i}.jpg`
+  img.src = `../images/kamae/bati_bad${i}.jpg`;
+};
+
+// 画像ごとにコメントを付け加える
+const changeText = (number) => {
+  switch (number) {
+    case 0:
+      kaisetu.textContent = "バチとバチの間にすき間が空いている。";
+      break;
+    case 1:
+      kaisetu.textContent = "バチの先が下を向いている。";
+      break;
+  }
 };
